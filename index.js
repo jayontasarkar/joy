d5b899bde3be0d01faf45e25e5f029aba516bd29
jayontasarkar <jayontasarkar025@gmail.com>
2020-10-07 22:12:39 +0600

    cleared console

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
+const clearAnyConsole = require('clear-any-console');
+// Clears the console
+clearAnyConsole();
+
 console.log(`
-I'm Jayonta Sarkar
+Hi! I'm Jayonta Sarkar.
 
 Passionate software engineer - Love to play with code.
+I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, as continuing to learn new languages and development techniques are important to me.
 
 🌎 Twitter: https://twitter.com/jayontasarkar
 🌓 Github: https://github.com/jayontasarkar
