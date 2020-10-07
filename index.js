29677345f7a1731cef994f7845dfdd224dced6f3
jayontasarkar <jayontasarkar025@gmail.com>
2020-10-07 21:48:41 +0600

    Release First Version 1.0.0

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+console.log(`
+I'm Jayonta Sarkar
+
+Passionate software engineer - Love to play with code.
+
+🌎 Twitter: https://twitter.com/jayontasarkar
+🌓 Github: https://github.com/jayontasarkar
+`);
